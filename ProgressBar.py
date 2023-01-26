04058de5db576b7ee1c5789080d83bcea781bba0
brungg <119693753+brungg@users.noreply.github.com>
2023-01-26 13:22:31 -0500

    Update ProgressBar.py

--- ProgressBar.py
+++ ProgressBar.py
@@ -4,10 +4,15 @@ from tkinter import ttk
 import AudioControls as ac
 
 class Bar:
-    def __init__(self, frame, songLength):
+    def __init__(self, frame, sl, fn):
         self.frame = frame
-        self.songLength = songLength
+        self.songLength = sl
+        self.fileName = fn
         self.var = 0
+
+        self.loop = None
+
+        self.check = True
 
         self.label = ttk.Label(self.frame, text=0)
         self.label.grid(column=3, row=0, sticky=W)
@@ -16,11 +21,25 @@ class Bar:
         self.bar.grid(column=0, row=0, columnspan=3, sticky=(W,E))
 
     def start(self):
-        self.var += 1
+        if self.check:
+            self.var += 1
+            self.bar['value'] = self.var
+            self.label['text'] = self.var
+            if self.bar['value'] >= self.songLength:
+                print("done")
+                ac.__stop__()
+                return
+            self.loop = self.frame.after(1000, self.start)
+
+    def stop(self):
+        if self.loop is not None:
+            self.bar['value'] = self.var
+            self.label['text'] = self.var
+            self.frame.after_cancel(self.loop)
+            self.loop = None
+            self.check = False
+        
+    def reset(self):
+        self.var = 0
         self.bar['value'] = self.var
         self.label['text'] = self.var
-        if self.bar['value'] >= self.songLength:
-            print("done")
-            ac.__stop__()
-            return
-        self.frame.after(1000, self.start)
