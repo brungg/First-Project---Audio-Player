1f825349a3021ba363435807a907f103a065d4fa
brungg <119693753+brungg@users.noreply.github.com>
2023-02-01 14:04:57 -0500

    Update AudioPlayer.py

--- AudioPlayer.py
+++ AudioPlayer.py
@@ -4,7 +4,7 @@ import AudioControls as ac
 import OpenFile as of
 import ProgressBar as pb
 
-root = Tk();
+root = Tk()
 root.title("Audio")
 
 frame = ttk.Frame(root, padding="5 5 15 15")
@@ -14,41 +14,57 @@ root.rowconfigure(0, weight=1)
 
 songLength = 10
 
-fileName = ""
+fileName = []
 
 bar = None
 
 def __getFile__():
-    global fileName, bar
-    fileName = of.getFile()
+    global fileName
+    f = of.getFile()
+    fileName.append(f)
+    i = f.split("/")
+    box.insert(box.size(), i[-1][:-4])
 def play():
     global bar
-    bar = pb.Bar(frame, ac.__length__(box.get(box.index(box.curselection()))), box.get(box.index(box.curselection())))
+    bar = pb.Bar(frame, ac.__length__(fileName[box.index(box.curselection())]), fileName[box.index(box.curselection())])
     bar.start()
-    ac.__play__(box.get(box.index(box.curselection())))
+    ac.__play__(fileName[box.index(box.curselection())])
 def pause():
-    global bar
     bar.stop()
     ac.__pause__()
 def unpause():
-    global bar
     bar.restart()
     bar.start()
     ac.__unpause__()
 def stop():
-    global bar
     bar.stop()
     bar.reset()
     ac.__stop__()
-def add():
-    box.insert(box.size(), for fileName)
-#def delete():
-#    pass
+def delete():
+    box.delete(box.index(box.curselection()))
+    fileName.pop(box.index(box.curselection()))
+def moveup():
+    global fileName
+    f = fileName[box.index(box.curselection())]
+    fileName.pop(box.index(box.curselection()))
+    fileName.insert(box.index(box.curselection()) - 1, f)
+    print(fileName)
+    box.delete(box.index(box.curselection()))
+    i = f.split("/")
+    box.insert(fileName.index(f), i[-1][:-4])
+def movedown():
+    global fileName
+    f = fileName[box.index(box.curselection())]
+    fileName.pop(box.index(box.curselection()))
+    fileName.insert(box.index(box.curselection()) + 1, f)
+    print(fileName)
+    box.delete(box.index(box.curselection()))
+    i = f.split("/")
+    box.insert(fileName.index(f), i[-1][:-4])
 
 def buttons(txt, cmd, c, r): ttk.Button(frame, text=txt, command=cmd).grid(column=c, row=r, sticky=(W, E))
 
-choice = StringVar()
-box = Listbox(frame, listvariable=choice)
+box = Listbox(frame)
 box.grid(column=4, columnspan=3, row=0)
 
 def main(): 
@@ -56,8 +72,9 @@ def main():
     buttons("pause", pause, 2, 1)
     buttons("unpause", unpause, 3, 1)
     buttons("stop/reset", stop, 4, 1)
-    buttons("add", add, 1, 2)
-    #buttons("delete", delete, 2, 2)
+    buttons("delete", delete, 1, 2)
+    buttons("↑", moveup, 2, 2)
+    buttons("↓", movedown, 2, 3)
 
     buttons("open file", __getFile__, 0, 1)
 
